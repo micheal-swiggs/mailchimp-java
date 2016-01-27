eb5bdffacc9719989175d620441bd24fb642a192
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2016-01-27 11:19:55 +0200

    Revert "Implement OAuthReuestInterceptor"
    This reverts commit 49bdab2ef0a0f7f5c1bbd3f807c7c693386c88ad.

--- src/main/java/com/mailchimp/MailChimpFactory.java
+++ src/main/java/com/mailchimp/MailChimpFactory.java
@@ -1,6 +1,5 @@
 package com.mailchimp;
 
-import com.mailchimp.auth.OAuthRequestInterceptor;
 import feign.*;
 import feign.jackson.*;
 import feign.auth.BasicAuthRequestInterceptor;
@@ -13,23 +12,13 @@ public class MailChimpFactory
         return "https://"+i+".api.mailchimp.com";
     }
 
-    public static MailChimpClient createWithBasicAuth (String apiKey, String apiBase)
+    public static MailChimpClient create (String apiKey, String apiBase)
     {
         MailChimpClient mailChimp = Feign.builder()
             .decoder(new JacksonDecoder())
             .encoder(new JacksonEncoder())
-            .requestInterceptor(new BasicAuthRequestInterceptor("anyString", apiKey))
+            .requestInterceptor(new BasicAuthRequestInterceptor("anyString", apiKey ))
             .target(MailChimpClient.class, mailChimpApi(apiBase));
-        return mailChimp;
-    }
-
-    public static MailChimpClient createWithOAuth (String accessToken, String apiBase)
-    {
-        MailChimpClient mailChimp = Feign.builder()
-                .decoder(new JacksonDecoder())
-                .encoder(new JacksonEncoder())
-                .requestInterceptor(new OAuthRequestInterceptor(accessToken))
-                .target(MailChimpClient.class, mailChimpApi(apiBase));
         return mailChimp;
     }
 }
