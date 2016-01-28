575d65556138c1a6853d46ddba041c919ae28a6d
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2016-01-28 14:41:41 +0200

    Enables List creation and creating multiple members with a batch.

--- src/main/java/com/mailchimp/domain/ListDefault.java
+++ src/main/java/com/mailchimp/domain/ListDefault.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
-import java.util.List;
-
 /**
  * @author Micheal Swiggs
  */
@@ -27,4 +25,22 @@ public class ListDefault
 
     @JsonProperty(value = JsonConstants.STATS)
     private ListStats stats;
+
+    @JsonProperty(value = JsonConstants.ID)
+    private String id;
+
+    @JsonProperty(value = JsonConstants.NAME)
+    private String name;
+
+    @JsonProperty(value = JsonConstants.CONTACT)
+    private Contact contact;
+
+    @JsonProperty(value = JsonConstants.PERMISSION_REMINDER)
+    private String permissionReminder;
+
+    @JsonProperty(value = JsonConstants.CAMPAIGN_DETAILS)
+    private CampaignDefaults campaignDefaults;
+
+    @JsonProperty(value = JsonConstants.EMAIL_TYPE_OPTION)
+    private Boolean emailTypeOption;
 }
