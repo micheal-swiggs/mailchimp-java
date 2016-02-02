af289969e3fd9d315e820e75bfb88227cf1c80ac
Doug Hagan <doug@stocksoftware.com.au>
2016-02-02 17:19:54 +1100

    Added removeMember() method. Removed public from interface methods.

--- src/main/java/com/mailchimp/MailChimpClient.java
+++ src/main/java/com/mailchimp/MailChimpClient.java
@@ -13,35 +13,38 @@ public interface MailChimpClient
     //public Object callList(@Param("list-id") String listId);
 
     @RequestLine("GET /3.0/lists/{list-id}/members/{user-id}")
-    public MemberDefault getListMember(@Param("list-id") String listId, @Param("user-id") String userId);
+    MemberDefault getListMember(@Param("list-id") String listId, @Param("user-id") String userId);
 
     @RequestLine("POST /3.0/lists/{list-id}/members")
-    public MemberDefault createMember(@Param("list-id") String listId, MemberCreate memberCreate);
+    MemberDefault createMember(@Param("list-id") String listId, MemberCreate memberCreate);
 
     @RequestLine("PUT /3.0/lists/{list-id}/members/{subscriber-hash}")
-    public MemberDefault updateMember(@Param("list-id") String listId, @Param("subscriber-hash") String subscriberHash, MemberDefault memberDefault);
+    MemberDefault updateMember(@Param("list-id") String listId, @Param("subscriber-hash") String subscriberHash, MemberDefault memberDefault);
+
+    @RequestLine("DELETE /3.0/lists/{list-id}/members/{subscriber-hash}")
+    void removeMember(@Param("list-id") String listId, @Param("subscriber-hash") String subscriberHash);
 
     @RequestLine("POST /3.0/lists")
-    public ListDefault createList(ListDefault listDefault);
+    ListDefault createList(ListDefault listDefault);
 
     @RequestLine("GET /3.0/lists/{list-id}")
-    public ListDefault getMembersList(@Param("list-id") String listId);
+    ListDefault getMembersList(@Param("list-id") String listId);
 
     @RequestLine("GET /3.0/lists/{list-id}/members")
-    public MemberListDefault getAllListMembers(@Param("list-id") String listId);
+    MemberListDefault getAllListMembers(@Param("list-id") String listId);
 
     @RequestLine("GET /3.0/lists/{list-id}/members?offset={offset}&count={count}")
-    public MemberListDefault getPagedListMembers(@Param("list-id") String listId, @Param("offset") Integer offset, @Param("count") Integer count);
+    MemberListDefault getPagedListMembers(@Param("list-id") String listId, @Param("offset") Integer offset, @Param("count") Integer count);
 
     @RequestLine("GET /3.0/lists/{list-id}/merge-fields?count={count}")
-    public MergeFieldsDefault getListMergeFields(@Param("list-id") String listId, @Param("count") Integer count);
+    MergeFieldsDefault getListMergeFields(@Param("list-id") String listId, @Param("count") Integer count);
 
     @RequestLine("POST /3.0/lists/{list-id}/merge-fields")
-    public MergeField createMergeField(@Param("list-id") String listId, MergeField mergeField);
+    MergeField createMergeField(@Param("list-id") String listId, MergeField mergeField);
 
     @RequestLine("POST /3.0/batches")
-    public Batch createMembersBatch(MemberCreateBatch batch);
+    Batch createMembersBatch(MemberCreateBatch batch);
 
     @RequestLine("GET /3.0/batches/{batch-id}")
-    public Batch getBatch(@Param("batch-id") String batchId);
+    Batch getBatch(@Param("batch-id") String batchId);
 }
