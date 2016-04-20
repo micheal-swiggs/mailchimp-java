d13985d75350a753d0a56ff2c359382e9697a6fb
nolan <nolan@deepmarkit.com>
2016-04-20 14:25:06 -0600

    Testing config of lombok

--- src/main/java/com/mailchimp/domain/AccountDefault.java
+++ src/main/java/com/mailchimp/domain/AccountDefault.java
@@ -12,6 +12,9 @@ public class AccountDefault {
     @JsonProperty(value = JsonConstants.ACCOUNT_NAME)
     private String accountName;
 
+    @JsonProperty(value = JsonConstants.EMAIL)
+    private String email;
+
     @JsonProperty(value = JsonConstants.CONTACT)
     private Contact contact;
 }
