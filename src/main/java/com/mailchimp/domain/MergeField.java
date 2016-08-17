bc7b9bccfb428948d66fd253d417c8d7993b9b4b
nolan <nolan@deepmarkit.com>
2016-08-17 14:43:01 -0600

    testing default value

--- src/main/java/com/mailchimp/domain/MergeField.java
+++ src/main/java/com/mailchimp/domain/MergeField.java
@@ -13,12 +13,13 @@ public class MergeField
     {
     }
 
-    public MergeField(String name, MergeType type, String tag, boolean required)
+    public MergeField(String name, MergeType type, String tag, boolean required, Integer displayOrder)
     {
         this.name = name;
         this.type = type;
         this.tag = tag;
         this.required = required;
+        this.displayOrder = displayOrder;
     }
 
     @JsonProperty(value = JsonConstants.NAME)
@@ -32,4 +33,7 @@ public class MergeField
 
     @JsonProperty(value = JsonConstants.REQUIRED)
     private boolean required = true;
+
+    @JsonProperty(value = "display_order")
+    private Integer displayOrder;
 }
