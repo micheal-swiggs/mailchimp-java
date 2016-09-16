5baf2994a8ba11787892963edf2ba2538a7b402d
stevensnoeijen <steven.snoeijen@falk.nl>
2016-09-16 09:48:52 +0200

    fix: update date(de)serialiser

--- src/main/java/com/mailchimp/jackson/FlexDateDeserializer.java
+++ src/main/java/com/mailchimp/jackson/FlexDateDeserializer.java
@@ -1,15 +1,14 @@
 package com.mailchimp.jackson;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.std.DateDeserializers.DateDeserializer;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @see <a href="http://www.baeldung.com/jackson-serialize-dates">Jackon Custom date serialization</a>
@@ -19,28 +18,28 @@ import com.fasterxml.jackson.databind.deser.std.DateDeserializers.DateDeserializ
  */
 public final class FlexDateDeserializer extends JsonDeserializer<Date> {
 
-	@Override
-	public Date deserialize(final JsonParser parser, final DeserializationContext context) throws IOException, JsonProcessingException {
-		final String date = parser.getText();
-		try {
-			return getFormatter().parse(date);
-		}
-		catch (final ParseException ex) {
-			// Not worked, so let the default date serializer give it a try.
-			return DateDeserializer.instance.deserialize(parser, context);
-		}
-	}
+    @Override
+    public Date deserialize(final JsonParser parser, final DeserializationContext context) throws IOException, JsonProcessingException {
+        final String date = parser.getText();
+        try {
+            return getFormatter().parse(date);
+        } catch (final ParseException ex) {
+            // Not worked, so let the default date serializer give it a try.
+            return DateDeserializer.instance.deserialize(parser, context);
+        }
+    }
 
-	//
-	//
-	private static SimpleDateFormat getFormatter() {
-		return FormatHolder.INSTANCE;
-	}
+    //
+    //
+    private static SimpleDateFormat getFormatter() {
+        return FormatHolder.INSTANCE;
+    }
 
-	/**
-	 */
-	private interface FormatHolder {
-		SimpleDateFormat INSTANCE = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
-	}
+    /**
+     */
+    private interface FormatHolder {
+
+        SimpleDateFormat INSTANCE = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ");
+    }
 
 }
