5779f0c9c5f2d1abc8ff6bf62602ebe60c1d325a
Kim Sparrow <kim.sparrow@gimbal.com>
2018-10-30 21:06:23 -0700

    Removed @JsonNaming from Tag and Tags classes, which don't benefit from its use.

--- src/main/java/com/mailchimp/domain/Tag.java
+++ src/main/java/com/mailchimp/domain/Tag.java
@@ -1,14 +1,11 @@
 package com.mailchimp.domain;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 /**
  * @author kimvsparrow
  */
 @Data
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Tag {
 
     public enum TagStatus {
--- src/main/java/com/mailchimp/domain/Tags.java
+++ src/main/java/com/mailchimp/domain/Tags.java
@@ -1,7 +1,5 @@
 package com.mailchimp.domain;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -11,7 +9,6 @@ import java.util.List;
  * @author kimvsparrow
  */
 @Data
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Tags {
     private List<Tag> tags = new ArrayList<>();
 }
